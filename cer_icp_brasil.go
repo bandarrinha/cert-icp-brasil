0f6243247f83f2be334efc9c38ed98df2600d6e5
bandarrinha <bandarrinha@gmail.com>
2021-05-19 15:36:53 -0300

    Altera o código para extrair informações de certificados ICP-Brasil que não estejam no padrão e-CPF

--- cer_icp_brasil.go
+++ cer_icp_brasil.go
@@ -14,22 +14,14 @@ var (
 	oidDadosTituloEleitor = []int{2, 16, 76, 1, 3, 5}
 )
 
-// SubjectAlternativeName estrutura para realizar a leitura da extensão Subject Alterantive Name OID 2.5.29.17
-type SubjectAlternativeName struct {
-	OtherName1 OtherName `asn1:"tag:0"`
-	OtherName2 OtherName `asn1:"tag:0"`
-	OtherName3 OtherName `asn1:"tag:0"`
-}
-
 // OtherName strutura para realizar a leitura dos campos otherName definidos pela ICP-Brasil contendo dados do titular do certificado
 type OtherName struct {
 	OID      asn1.ObjectIdentifier
-	Conteudo OctetString `asn1:"tag:0"`
+	Conteudo `asn1:"tag:0"`
 }
 
-// OctetString formato ASN.1 OCTET STRING
-type OctetString struct {
-	Value interface{}
+type Conteudo struct {
+	Valor interface{}
 }
 
 // PessoaFisica estrutura que representa os dados de uma pessoa física contidos em um certificado icp-brasil
@@ -41,6 +33,7 @@ type PessoaFisica struct {
 	RG             RegistroGeral
 	CEI            string
 	TituloEleitor  TituloEleitor
+	Email          string
 }
 
 // TituloEleitor estrutura que representa os dados do Título de Eleitor
@@ -61,12 +54,13 @@ type RegistroGeral struct {
 
 // ParseDadosPessoaFisicaFromCertificado função que extrai os dados do titular de um certificado ICP-Brasil Pessoa Física
 func ParseDadosPessoaFisicaFromCertificado(cert *x509.Certificate) (p PessoaFisica, err error) {
-	var san SubjectAlternativeName
+	var san asn1.RawValue
 	if strings.Contains(cert.Subject.CommonName, ":") {
 		p.Nome = strings.Split(cert.Subject.CommonName, ":")[0]
 		for _, ou := range cert.Subject.OrganizationalUnit {
 			if strings.Contains(ou, "e-CPF") {
 				p.CPF = strings.Split(cert.Subject.CommonName, ":")[1]
+				break
 			}
 		}
 	} else {
@@ -75,80 +69,71 @@ func ParseDadosPessoaFisicaFromCertificado(cert *x509.Certificate) (p PessoaFisi
 	for _, ext := range cert.Extensions {
 		if ext.Id.Equal(oidSAN) {
 			_, err = asn1.Unmarshal(ext.Value, &san)
-			break
+			if err != nil {
+				return PessoaFisica{}, err
+			}
+			rest := san.Bytes
+			for len(rest) > 0 && err == nil {
+				rest, err = asn1.Unmarshal(rest, &san)
+				switch san.Tag {
+				case 0:
+					var otherName OtherName
+					_, err := asn1.UnmarshalWithParams(san.FullBytes, &otherName, "tag:0")
+					if err != nil {
+						return PessoaFisica{}, err
+					}
+					var valor string
+					switch otherName.Conteudo.Valor.(type) {
+					case []byte:
+						valor = string(otherName.Conteudo.Valor.([]byte))
+					case string:
+						valor = otherName.Conteudo.Valor.(string)
+					}
+					if valor != "" {
+						conteudo := string(valor)
+						switch {
+						case otherName.OID.Equal(oidDadosPessoaFisica):
+							p.DataNascimento, err = time.Parse("02012006", conteudo[0:8])
+							if err != nil {
+								return PessoaFisica{}, err
+							}
+							if p.CPF == "" && conteudo[8:19] != "00000000000" {
+								p.CPF = conteudo[8:19]
+							}
+							if conteudo[19:30] != "00000000000" {
+								p.NIS = conteudo[19:30]
+							}
+							if conteudo[30:45] != "000000000000000" {
+								p.RG.Numero = strings.TrimLeft(conteudo[30:45], "0")
+								p.RG.UF = conteudo[len(conteudo)-2:]
+								p.RG.OrgaoExpeditor = conteudo[45 : len(conteudo)-2]
+							}
+						case otherName.OID.Equal(oidDadosCEI):
+							if conteudo != "000000000000" {
+								p.CEI = conteudo
+							}
+						case otherName.OID.Equal(oidDadosTituloEleitor):
+							if conteudo[0:12] != "000000000000" {
+								p.TituloEleitor.Inscricao = conteudo[0:12]
+								p.TituloEleitor.ZonaEleitoral = conteudo[12:15]
+								p.TituloEleitor.Secao = conteudo[15:19]
+								p.TituloEleitor.UF = conteudo[len(conteudo)-2:]
+								p.TituloEleitor.Municipio = conteudo[19 : len(conteudo)-2]
+							}
+						}
+					}
+				case 1:
+					_, err = asn1.UnmarshalWithParams(san.FullBytes, &p.Email, "tag:1,ia5")
+					if err != nil {
+						return PessoaFisica{}, err
+					}
+				}
+			}
+
 		}
 	}
 	if err != nil {
 		return PessoaFisica{}, err
 	}
-
-	var conteudo []byte
-	var str string
-	var ok bool
-	switch {
-	case san.OtherName1.OID.Equal(oidDadosPessoaFisica):
-		conteudo, ok = san.OtherName1.Conteudo.Value.([]byte)
-	case san.OtherName2.OID.Equal(oidDadosPessoaFisica):
-		conteudo, ok = san.OtherName2.Conteudo.Value.([]byte)
-	case san.OtherName3.OID.Equal(oidDadosPessoaFisica):
-		conteudo, ok = san.OtherName3.Conteudo.Value.([]byte)
-	}
-	if ok {
-		str = string(conteudo)
-	}
-	if ok && str != "" {
-		p.DataNascimento, err = time.Parse("02012006", str[0:8])
-		if err != nil {
-			return PessoaFisica{}, err
-		}
-		if p.CPF == "" && str[8:19] != "00000000000" {
-			p.CPF = str[8:19]
-		}
-		if str[19:30] != "00000000000" {
-			p.NIS = str[19:30]
-		}
-		if str[30:45] != "000000000000000" {
-			p.RG.Numero = strings.TrimLeft(str[30:45], "0")
-			p.RG.UF = str[len(str)-2:]
-			p.RG.OrgaoExpeditor = str[45 : len(str)-2]
-		}
-	}
-
-	switch {
-	case san.OtherName1.OID.Equal(oidDadosCEI):
-		conteudo, ok = san.OtherName1.Conteudo.Value.([]byte)
-	case san.OtherName2.OID.Equal(oidDadosCEI):
-		conteudo, ok = san.OtherName2.Conteudo.Value.([]byte)
-	case san.OtherName3.OID.Equal(oidDadosCEI):
-		conteudo, ok = san.OtherName3.Conteudo.Value.([]byte)
-	}
-	str = ""
-	if ok {
-		str = string(conteudo)
-	}
-	if str != "" && str != "000000000000" {
-		p.CEI = str
-	}
-
-	switch {
-	case san.OtherName1.OID.Equal(oidDadosTituloEleitor):
-		conteudo, ok = san.OtherName1.Conteudo.Value.([]byte)
-	case san.OtherName2.OID.Equal(oidDadosTituloEleitor):
-		conteudo, ok = san.OtherName2.Conteudo.Value.([]byte)
-	case san.OtherName3.OID.Equal(oidDadosTituloEleitor):
-		conteudo, ok = san.OtherName3.Conteudo.Value.([]byte)
-	}
-	str = ""
-	if ok {
-		str = string(conteudo)
-	}
-	if str != "" && str[0:12] != "000000000000" {
-		p.TituloEleitor.Inscricao = str[0:12]
-		p.TituloEleitor.ZonaEleitoral = str[12:15]
-		p.TituloEleitor.Secao = str[15:19]
-		p.TituloEleitor.UF = str[len(str)-2:]
-		p.TituloEleitor.Municipio = str[19 : len(str)-2]
-	}
-
 	return p, nil
 }
